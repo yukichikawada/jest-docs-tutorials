03dd1e1a76ee9ce6683b8b7c8de69df8f0cd7ad0
Yukichi Kawada <yukichikawada@gmail.com>
2018-06-26 22:27:52 -0700

    Add setup for snapshot tutorial

---
+++ snapshots/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import LinkComponent from './components/LinkComponent';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello world</h1>
+        <LinkComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ snapshots/src/components/LinkComponent.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const STATUS = {
+  HOVERED: 'hovered',
+  NORMAL:  'normal',
+};
+
+export default class LinkComponent extends React.Component {
+  constructor() {
+    super();
+
+    this._onMouseEnter = this._onMouseEnter.bind(this);
+    this._onMouseLeave = this._onMouseLeave.bind(this);
+
+    this.state = {
+      status: STATUS.NORMAL,
+    };
+  }
+
+  _onMouseEnter() {
+    this.setState({status: STATUS.HOVERED});
+  }
+
+  _onMouseLeave() {
+    this.setState({status: STATUS.NORMAL});
+  }
+
+  render() {
+    return (
+      <a className={this.state.status}
+        href={this.props.page || '#'}
+        onMouseEnter={this._onMouseEnter}
+        onMouseLeave={this._onMouseLeave}
+      >
+        LinkComponent
+      </a>
+    );
+  }
+}
